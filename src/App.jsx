6537c3de8dca78ab8974c7d18b8e363195f7d416
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2025-01-10 21:36:26 +0000

    implement project creation toggle in App layout with updated SideBar and NoProjectInfo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import { useState } from 'react'
 import NoProjectInfo from "./components/NoProjectInfo";
 import ProjectForm from "./components/ProjectForm";
 import SideBar from "./components/SideBar";
 
 function App() {
+  const [createProject, setCreateProject] = useState(false);
   return (
     <>
       <main className="h-screen my-8 flex gap-8">
-        <SideBar />
-        {/*<NoProjectInfo />*/}
-        <ProjectForm />
+        <SideBar setCreateProject={setCreateProject} />
+        {createProject ? <ProjectForm /> : <NoProjectInfo setCreateProject={setCreateProject} />}
       </main>
     </>
   );
