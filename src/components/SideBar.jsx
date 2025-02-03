9c2d8cfd61c17cadc336108d75a16f33538f2676
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2025-02-03 20:42:50 +0000

    update SideBar component to directly set project creation state on button click

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,7 +4,7 @@ export default function SideBar({ setCreateProject }) {
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
-      <button onClick={() => setCreateProject((oldCreateProject) => { return !oldCreateProject})} className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+      <button onClick={() => setCreateProject(true)} className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
         + Add Project
       </button>
       <ul className="mt-8">
