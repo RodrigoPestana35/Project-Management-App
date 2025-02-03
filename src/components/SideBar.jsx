ed91780b0f8da5926e1c1ff865617763c9197ebe
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2025-02-03 23:32:23 +0000

    add SideBar component and update App to include it

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+    return(
+        <aside>
+            <h2>Your Projects</h2>
+            <div>
+                <button>+ Add Project</button>
+            </div>
+            <ul>
+
+            </ul>
+        </aside>
+    )
+}
