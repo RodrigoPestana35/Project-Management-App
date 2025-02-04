da0b19b9b34ce2174f5ce2bad7d1d7faec4f1acb
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2025-02-04 00:16:06 +0000

    refactor App, SideBar, and NoneProjectSelected components to manage project selection state and improve UI interaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
+import NoneProjectSelected from "./components/NoneProjectSelected";
 import SideBar from "./components/SideBar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,  //undefined significa que não estamos a fazer nada
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return{
+        ...prevState,
+        selectedProject: null,  //null significa que estamos a adicionar um novo projeto
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProject === null){
+    content = <NewProject />
+  } else if(projectsState.selectedProject === undefined){
+    content = <NoneProjectSelected onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      <NewProject />
+      <SideBar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
