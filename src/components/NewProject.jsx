e23935b69103becd223bd52d367c69bc04d80191
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2025-02-04 00:56:12 +0000

    add Modal component for input validation feedback in NewProject

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,10 +1,13 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({ onAdd }) {
     const titleRef = useRef();
     const descriptionRef = useRef();
     const dueDateRef = useRef();
+
+    const modal = useRef();
 
     function handleSave(){
         const title = titleRef.current.value;
@@ -12,6 +15,10 @@ export default function NewProject({ onAdd }) {
         const dueDate = dueDateRef.current.value;
 
         //validation
+        if(title.trim() === '' || description.trim() === '' || dueDate.trim() === ''){
+            modal.current.open();
+            return;
+        }
 
         onAdd({
             title: title,
@@ -21,6 +28,12 @@ export default function NewProject({ onAdd }) {
     }
 
   return (
+    <>
+    <Modal ref={modal} buttonCaption="Close">
+        <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+        <p className='text-stone-600 mb-4'>Oops... looks like you forgot to enter a value.</p>
+        <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+    </Modal>
     <div className="w-[32rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
@@ -38,5 +51,6 @@ export default function NewProject({ onAdd }) {
         <Input type='date' ref={dueDateRef} label="Due Date" />
       </div>
     </div>
+    </>
   );
 }
